a9b5ee97ce4a7af7e2f7ac5b1ac72b4aab1d6e37
estellehue <55542929+estellehue@users.noreply.github.com>
2021-10-18 16:04:28 +0200

    Rearrange

--- RestaurantMenu/Different/js/views/promiseNoData.js
+++
@@ -1,7 +0,0 @@
-function promiseNoData(promise, data, error, h){  
-     return  !promise && "no data"     // case "0"  
-           || error && <h1>{error}</h1>   
-           || !data && <img src="http://www.csc.kth.se/~cristi/loading.gif"/>     // case 1
-}
-
-           //add h
