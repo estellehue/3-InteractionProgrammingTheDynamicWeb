09fcf3f527a54494f501683b401d76644109049a
estellehue <55542929+estellehue@users.noreply.github.com>
2021-10-22 16:10:32 +0200

    Removed Assignment

--- RestaurantMenu/Different/reactjs/show.js
+++
@@ -1,8 +0,0 @@
-function Show({hash, children}){  
-   const [, setRoute]= React.useState(window.location.hash);
-   React.useEffect(()=> 
-     window.addEventListener("hashchange", 
-                ()=> setRoute({}), [])); 
-            
-   return hash===window.location.hash?children: false;
-}   
